12ad7ee9781139a5878fdbf09a4c429bfc3b416b
MouadDv <mouaddv@gmail.com>
2022-02-26 06:18:47 +0100

    binary tree

--- map/map.hpp
+++ map/map.hpp
@@ -2,43 +2,86 @@
 #ifndef MAP_CLASS
 #define MAP_CLASS
 
+#include "pair.hpp"
 #include <iostream>
 
-template <typename F, typename S>
-struct _tree
+namespace ft
 {
-private:
-	struct tree *root;
-	struct tree *left;
-	struct tree *right;
-	struct tree *parent;
-	std::pair<F, S> val;
+
+template <class T1, class T2>
+struct tree {
+	tree<T1, T2> *r;
+	tree<T1, T2> *l;
+	//tree<T1, T2> *parent;
+    pair<const T1, T2> val;
+
+    tree(const pair<const T1, T2> &p): val(p), r(0), l(0) {}
+};
+
+template <typename F, typename S>
+class Avl {
 public:
-	_tree(){
-	}
-	_tree(std::pair<F, S> s)
-	{
-		root = this;
-		left = nullptr;
-		right = nullptr;
-		parent = nullptr;
-		val = s;
-	}
-	void insert(std::pair<F, S> s)
-	{
-		struct tree *next = new struct tree(s);
-		if (s.first > val.first)
-			this->left = next;
-		else
-			this->right = next;
-		next->root = this;
-		next->parent = this;
-	}
-	void print( void )
-	{
-		struct tree *tmp = this->root;
-	}
+    tree<F, S> *nodes;
+    tree<F, S> *root;
+public:
+	Avl(): nodes(0), root(0) {
+    }
+    void insert(tree<F, S> **pos,pair<const F, S> &p)
+    {
+        if ((*pos) == nullptr)
+        {
+            (*pos) = new tree<F, S>(p);
+            //if(this->root == nullptr)
+            //    this->root = pos;
+            //std::cout << "Element: " << pos->val.first << "\n";
+        }
+        else
+        {
+            if (p < (*pos)->val)
+                insert(&((*pos)->l), p);
+            else
+                insert(&((*pos)->r), p);
+        }
+    }
+	~Avl(){}
 };
 
 
+using namespace std;
+#define COUNT 10
+
+template <class F, class S>
+void print2DUtil(ft::tree<F, S> *root, int space)
+{
+    // Base case
+    if (root == NULL)
+        return;
+
+    // Increase distance between levels
+    space += COUNT;
+
+    // Process right child first
+    print2DUtil(root->r, space);
+
+    // Print current node after space
+    // count
+    cout<<endl;
+    for (int i = COUNT; i < space; i++)
+        cout<<" ";
+    cout<<root->val.first<<"\n";
+
+    // Process left child
+    print2DUtil(root->l, space);
+}
+
+// Wrapper over print2DUtil()
+template<typename F, typename S>
+
+void print2D(ft::tree<F, S> *root)
+{
+    // Pass initial space count as 0
+    print2DUtil(root, 0);
+}
+
+}
 #endif
---
+++ map/pair.hpp
@@ -0,0 +1,77 @@
+#pragma once
+#ifndef PAIR_CLASS
+#define PAIR_CLASS
+
+namespace ft
+{
+
+
+  template <class T1, class T2>
+  class pair {
+  public:
+    T1 first;
+    T2 second;
+  public:
+    pair(){}
+    template<class U, class V>
+    pair (const pair<U,V>& c): first(c.first), second(c.second)
+    {
+    }
+    pair (const T1& a, const T2& b): first(a), second(b)
+    {
+
+    }
+    template<class U, class V>
+    pair &operator=(const pair<U, V>& c)
+    {
+      this->first = c.first;
+      this->second = c.second;
+      return (*this);
+    }
+    ~pair()
+    {
+
+    }
+  };
+
+  template <class T1, class T2>
+  bool operator==(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+  {
+    return (lhs.first == rhs.first && lhs.second == rhs.second);
+  }
+  template <class T1, class T2>
+  bool operator!=(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+  {
+    return !(lhs == rhs);
+  }
+
+  template <class T1, class T2>
+  bool operator< (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+  {
+    return (lhs.first<rhs.first || (!(rhs.first<lhs.first) && lhs.second<rhs.second));
+  }
+  template <class T1, class T2>
+  bool operator<=(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+  {
+    return !(rhs<lhs);
+  }
+  template <class T1, class T2>
+  bool operator> (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+  {
+    return (rhs<lhs);
+  }
+  template <class T1, class T2>
+  bool operator>=(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+  {
+    return !(lhs<rhs);
+  }
+
+  template <class T1,class T2>
+  pair<T1,T2> make_pair (T1 x, T2 y)
+  {
+    return ( pair<T1,T2>(x,y) );
+  }
+
+}
+
+#endif
