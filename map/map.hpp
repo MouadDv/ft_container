3da100b66e58d661ccdb96875146c3db8cfd343b
MouadDv <mouaddv@gmail.com>
2022-03-08 04:12:17 +0100

    map typedefs

--- map/map.hpp
+++ map/map.hpp
@@ -2,22 +2,33 @@
 #ifndef MAP_CLASS
 #define MAP_CLASS
 
+#include <iostream>
 #include "pair.hpp"
+#include "avl.hpp"
+#include "bidirectional_iterator.hpp"
 
 namespace ft
 {
   template < class Key,                                     // map::key_type
-           class T                                       // map::mapped_type
-           //class Compare = less<Key>,                     // map::key_compare
-           //class Alloc = allocator<pair<const Key,T> >    // map::allocator_type
+           class T,                                       // map::mapped_type
+           class Compare = std::less<Key>,                     // map::key_compare
+           class Alloc = std::allocator<ft::pair<const Key,T> >    // map::allocator_type
            >
   class map {
   public:
-    typedef ft::pair<const Key, T> value_type;
-    typedef map* pointer;
-    typedef map& reference;
-  public:
+    typedef Key                                    key_type;
+    typedef T                                      mapped_type;
+    typedef Compare                                key_compare;
+    typedef Alloc                                  allocator_type;
+    typedef ft::pair<const Key, T>                 value_type;
+    typedef map*                                   pointer;
+    typedef map&                                   reference;
+    typedef std::bidirectional_iterator_tag        iterator_category;
 
+  private:
+    Avl<key_type, mapped_type> tree;
+
+  public:
     map();
     virtual ~map();
 
